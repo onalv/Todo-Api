da1702069dd6539c43b0f4df36170f0c7265596a
navao <omarnavaalvarez@gmail.com>
2016-11-28 22:09:49 +0100

    Add hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,18 +1,35 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
-			allowNull: false, 
+			allowNull: false,
 			unique: true,
 			validate: {
 				isEmail: true
-			}			
-		}, 
+			}
+		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
 		password: {
-			type: DataTypes.STRING,
+			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
@@ -21,6 +38,12 @@ module.exports = function(sequelize, DataTypes) {
 				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
