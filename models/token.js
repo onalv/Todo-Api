3a653f798184e4af0d264e2e64131b422e26fbbb
navao <omarnavaalvarez@gmail.com>
2016-11-30 22:57:13 +0100

    Adding DELETE /users/login

---
+++ models/token.js
@@ -0,0 +1,21 @@
+var cryptojs = require('crypto-js');
+
+module.exports = function(sequelize, DataTypes) {
+
+	return sequelize.define('token', {
+		token: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1]
+			},
+			set: function (value) {
+				var hash = cryptojs.MD5(value).toString();
+
+				this.setDataValue('token', value);
+				this.setDataValue('tokenHash', hash);
+			}
+		},
+		tokenHash: DataTypes.STRING
+	});
+};	
